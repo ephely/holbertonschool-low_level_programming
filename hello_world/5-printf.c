1b423f13faf4deedf6c92cbcb3aeb1dc655bf916
Ephely <ephely@gmail.com>
2024-10-21 12:36:07 +0000

    Vista

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
+≈yint main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
