221cdf57254c2f9271f9ab423809fcdb0004e9b7
Ephely <ephely@gmail.com>
2024-10-21 15:01:33 +0000

    FREE THUG

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,13 +11,12 @@ int last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10
 
 if (n > 5)
-printf("%d is greater than 5\n", n);
+printf("%d is greater than 5\n", n, n % 10);
 else if (n == 0)
-printf("%d is 0\n", n;
+printf("%d is 0\n", n, n % 10;
 else
-printf("%d is is less than 6 and not 0\n", n);
+printf("%d is is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
