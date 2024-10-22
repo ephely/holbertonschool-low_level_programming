3629cc64a4797de2da1c4a556a138328cac0049b
Ephely <ephely@gmail.com>
2024-10-22 09:12:53 +0000

    alp

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp;
+
+for (alp = 'A'; alp <= 'B'; alp++)
+{
+putchar(alp);
+}
+
+putchar('\n');
+
+return (0);
+}
