e70072481fd7372629c5f2df5b28a87b6d2c1b7c
Ephely <ephely@gmail.com>
2024-10-22 14:57:44 +0000

    Wejdene

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char numb;
+char alp;
+
+for (numb = '0'; numb  <= '9'; alp++)
+{
+putchar(numb);
+}
+for (alp = 'a'; alp <= 'f'; alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+
+return (0);
+}
