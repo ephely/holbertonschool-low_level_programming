95a5d29ee38cdef05fdb670b11fd15fb860ad750
Ephely <ephely@gmail.com>
2024-10-22 12:12:25 +0000

    aBc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,14 @@ int main(void)
 {
 char alp;
 
-for (alp = 'a'; alp <= 'B'; alp++)
+for (alp = 'a'; alp <= 'z'; alp++)
 {
 putchar(alp);
 }
-
+for (alp = 'A'; alp <= 'Z'; alp++)
+{
+putchar(alp);
+}
 putchar('\n');
 
 return (0);
