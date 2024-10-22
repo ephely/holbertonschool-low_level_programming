1dea5ecd202fe9340dfbc777258cc2b9f9dc1b84
Ephely <ephely@gmail.com>
2024-10-22 13:40:41 +0000

    Kurt Cobain

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char numb;
+
+for (numb = '0'; alp <= '9'; numb++)
+{
+putchar(numb);
+}
+
+putchar('\n');
+
+return (0);
+}
+
