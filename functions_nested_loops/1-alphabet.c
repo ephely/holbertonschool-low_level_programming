6857b48d06c97256043d67adee30459f7254ff19
Ephely <ephely1@gmail.com>
2024-10-23 14:32:47 +0000

    abc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+char grr
+
+for (grr='a'; grr <= 'z'; grr++)
+{
+_putchar(grr);
+}
+return(0);
+}
