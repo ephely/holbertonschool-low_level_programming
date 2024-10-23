3217d5769e3001516580425d155dc4a53beedef2
Ephely <ephely1@gmail.com>
2024-10-23 15:12:15 +0000

    Betty Betty Betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
-/*
-* main - Entry point
+/**
+* print_alphabet - print the alphabet
 *
-* Return: Always 0 (Success)
 */
 
-int main(void)
 void print_alphabet(void)
 {
-char grr
 
-for (grr ='a'; grr <= 'z'; grr++)
+char grr;
+
+for (grr = 'a'; grr <= 'z'; grr++)
 {
 _putchar(grr);
 }
 _putchar('\n');
-
-return(0);
 }
