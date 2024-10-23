686da21a4704a0441224465a7455f92520e63f39
Ephely <ephely1@gmail.com>
2024-10-23 12:22:37 +0000

    LLCE

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,10 +4,14 @@
 *
 * Return: Always 0 (Success)
 *
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
-
+int _putchar(char c);
 int main(void)
-
 {
 
 _putchar('_');
