0aaa4007b2174ae0f3b79f872f4dd74d079662e3
Ephely <ephely1@gmail.com>
2024-10-25 12:14:58 +0000

    lower & UPPER

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * _isalpha - checking lowercase and uppercase characters
-* Description: Write a function that check lowercase characters
+* Description: Write a function that check lowercase and uppercase characters
 * @c: The character to check
-* Return: 1 if the character is lowercase, 0 otherwise.
+* Return: 1 if the character is lowercase or uppercase, 0 otherwise.
 */
 int _isalpha(int c);
 {
