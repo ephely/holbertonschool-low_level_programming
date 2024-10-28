09c5b593341ffbc48e130e571cd29a893999c7ce
Ephely <ephely1@gmail.com>
2024-10-28 13:14:32 +0000

    mule

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - multiply
+* Write a function that multiplies two integers.
+* @a: 1st int
+* @b: 2nd int
+* Return: Value of the last digit of number
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
