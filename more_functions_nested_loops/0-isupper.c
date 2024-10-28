27c6c4a9d42cf86f806f50efa9a8b5332fdbf44c
Ephely <ephely1@gmail.com>
2024-10-28 09:58:01 +0000

    UPPER

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper≈ - checking uppercase characters
+* Description: Write a function that check uppercase characters
+* @c: The character to check
+* Return: 1 if the character is uppercase, 0 otherwise.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
