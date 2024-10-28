2f678579c07d2acb715a001585ff703373c617fa
Ephely <ephely1@gmail.com>
2024-10-28 13:24:17 +0000

    PM

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - print the alphabet
+*
+*/
+
+void print_numbers(void)
+{
+
+char faignon;
+
+for (faignon = '0'; faignon <= '9'; faignon++)
+{
+_putchar(faignon);
+}
+_putchar('\n');
+}
