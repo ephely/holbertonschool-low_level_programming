1aa24d716fdfba53d4e09b24b2c4a24653729f05
Ephely <ephely1@gmail.com>
2024-10-29 14:23:40 +0000

    HASHTAG

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* print_square - print squares w/ #
+* Write a function that prints a square, followed by a new line.
+*
+*
+*/
+
+void print_square(int size)
+{
+int gang;
+int gucci;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for(gang = 0; gang <= size; gang++)
+{
+for(gucci = 0; gucci <= size; gucci++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
