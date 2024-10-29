006ceaede9f71dc4f07b6bd27306ebf0182841e7
Ephely <ephely1@gmail.com>
2024-10-29 09:19:37 +0000

    LINES

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+* print_line - print lines for pleasure
+*
+* Write a function that draws a straight line in the terminal.
+*
+* @n: int of number of lines
+*/
 void print_line(int n)
 {
 int i = 0;
 if (n > 0)
 {
-while(i != n )
+while (i != n)
 {
 _putchar('_');
 i++;
