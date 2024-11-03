84f20ed5384e178757d14c9f237fe762d88e9dcc
Ephely <ephely1@gmail.com>
2024-11-03 17:46:48 +0000

    Array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,8 @@
 
 /**
 * print_array - prints numbers of an array of integers
-* Write a function that prints n elements of an array of integers, followed by a new line.
+* Write a function that prints n elements of an array of integers,
+* followed by a new line.
 * @a: This is the input array
 * @n: This is the lenght of the array
 * @rr: count
