d61a1ab031b02961504d8336b0623b58893d2c6e
Ephely <ephely1@gmail..com>
2024-11-04 14:52:48 +0000

    Rev Arr

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
 * Return: void
 */
 
-void reverse_array(int *a, int n) 
-{ 
-int temp;
-int i; 
-for (i = 0; i < n / 2; i++) 
+void reverse_array(int *a, int n)
 {
-temp = a[i]; 
-a[i] = a[n - 1 - i]; 
-a[n - 1 - i] = temp; 
-} 
+int temp;
+int i;
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
 }
