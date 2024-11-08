5c2779a5fa6854660d8b1685c313592dcc072c25
Ephely <ephely1@gmail.com>
2024-11-08 22:53:28 +0000

    noisrucer

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _print-rev_recursion - prints a string in reverse
+*
+* @s: char
+*
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
