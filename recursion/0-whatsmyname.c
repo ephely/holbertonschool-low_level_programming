d3571470c38064f481ee0c2dca190d7544a71a28
Ephely <ephely1@gmail.com>
2024-11-10 14:02:30 +0000

    My Name Is

--- recursion/0-whatsmyname.c
+++ recursion/0-whatsmyname.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+/**
+* main - program that prints its name
+*
+* @argc: int
+* @argv: char
+*
+* Return: the return
+*/
+
 int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 }
 return ('O');
 }
