1fc5581f7746b1a5c7dab0e3f54ecc54879eaf58
Ephely <ephely1@gmail.com>
2024-11-12 15:59:04 +0000

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*/
+
+char *_strdup(char *str)
+{
+int i;
+
+if (str = nul)
+{
+return (NULL);
+}
+for(i = 0; i != '\0'; i++
+gr = malloc(size * sizeof((str)+1));
+if (gr == NULL)
+{
+return (NULL);
+}
+
+}
