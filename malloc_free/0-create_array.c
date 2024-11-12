29c71b1c6e5321b5894b90ecbae7f4fbdeca4f92
Ephely <ephely1@gmail.com>
2024-11-12 15:40:19 +0000

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_array - creates an array of chars
+*
+* @size: unsigned int
+* @c: char
+*
+* Return: Always 0.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char *ar;
+unsigned int i = 0;
+
+ar = malloc(size * sizeof(c));
+
+while (i < size)
+{
+if (ar == NULL)
+{
+return (NULL);
+}
+ar[i] = c;
+i++;
+}
+return (my_array);
+}
