0c0c6726288bbe81f059066e1b534c64893b4539
Ephely <ephely1@gmail.com>
2024-11-14 14:03:50 +0000

    array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,18 +13,19 @@
 char *create_array(unsigned int size, char c)
 {
 char *ar;
-unsigned int i = 0;
+unsigned int i;
 
-ar = malloc(size * sizeof(c));
-
-while (i < size)
-{
-if (ar == NULL)
-{
+if (size == 0)
 return (NULL);
-}
+
+
+ar = malloc(size * sizeof(*ar));
+
+if (ar == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
 ar[i] = c;
-i++;
-}
+
 return (ar);
 }
