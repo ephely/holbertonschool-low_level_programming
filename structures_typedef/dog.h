2b024ce384afec8ecd2ca616aff14dd55eb79fc7
Ephely <ephely1@gmail.com>
2024-11-15 11:11:12 +0000

    WOOF

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - dog data
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
+*/
+
 typedef struct dog
 {
 char *name;
