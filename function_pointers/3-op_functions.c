5f175448853d845b2de10c93ccdc8155c98b2691
« Ephely » <ephely1@gmail.com>
2024-11-19 15:59:45 +0000

    c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,80 @@
+/**
+* op_add - add
+*
+* @a: int
+* @b: int
+*
+* Return: a + b
+*/
+
+
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - sub
+*
+* @a: int
+* @b: int
+*
+* Return: a + b
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - miltiply
+*
+* @a: int
+* @b: int
+*
+* Return: a * b
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - divide
+*
+* @a: int
+* @b: int
+*
+* Return: a / b
+*/
+
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+fprintf(stderr, "divided by zero\n");
+}
+return (a / b);
+}
+
+/**
+* op_mod - rest of a division
+*
+* @a: int
+* @b: int
+*
+* Return: a / b
+*/
+
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+fprintf(stderr, "divided by zero\n");
+return (-1);
+}
+return (a / b);
+}
