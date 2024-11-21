f9c659c8bcc6296b8cb34bbd54053b7836dcf938
« Ephely » <ephely1@gmail.com>
2024-11-21 15:01:42 +0000

    3...

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,10 +21,13 @@ op_t ops[] = {
 { NULL, NULL }
 };
 int i = 0;
-while (i < 5)
+
+while (ops[i].op != NULL)
 {
-if (ops[i].op[0] == s[0])
+if (strcmp(ops[i].op, s) == 0)
+{
 return (ops[i].f);
+}
 i++;
 }
 return (NULL);
