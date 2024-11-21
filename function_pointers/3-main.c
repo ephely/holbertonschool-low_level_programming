4c4dd41f0db868b3dffd5ba8c4c6253d8b4bbd71
« Ephely » <ephely1@gmail.com>
2024-11-21 14:47:21 +0000

    3 main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,31 +13,39 @@
 
 int main(int argc, char *argv[])
 {
+
 int a, b;
+int (*o)(int, int);
+
 int *o;
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-
+if (argv[2][1] != '\0')
 a = atoi(argv[1]);
 o = argv[2];
 b = atoi(argv[3]);
-
 if (get_op_func(o) == NULL || o[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
 
+o = get_op_func(argv[2]);
+if (o == NULL)
 if ((*o == '/' && b == 0) || (*o == '%' && b == 0))
 {
 printf("Error\n");
+exit(99);
 exit(100);
 }
 
+a = atoi(argv[1]);
+b = atoi(argv[3]);
 printf("%d\n", get_op_func(op)(a, b));
 
+printf("%d\n", o(a, b));
 return (0);
 }
