3f1f97930ce3fd67dfc4daaf5fdaffaf475c4d39
« Ephely » <ephely1@gmail.com>
2024-11-21 14:49:55 +0000

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
 * main - main
 *
@@ -10,42 +9,20 @@
 *
 * Return: 0
 */
-
 int main(int argc, char *argv[])
 {
-
-int a, b;
-int (*o)(int, int);
-
-int *o;
+int (*oprt)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (argv[2][1] != '\0')
-a = atoi(argv[1]);
-o = argv[2];
-b = atoi(argv[3]);
-if (get_op_func(o) == NULL || o[1] != '\0')
+oprt = get_op_func(argv[2]);
+if (!oprt)
 {
 printf("Error\n");
 exit(99);
 }
-
-o = get_op_func(argv[2]);
-if (o == NULL)
-if ((*o == '/' && b == 0) || (*o == '%' && b == 0))
-{
-printf("Error\n");
-exit(99);
-exit(100);
-}
-
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-printf("%d\n", get_op_func(op)(a, b));
-
-printf("%d\n", o(a, b));
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }
