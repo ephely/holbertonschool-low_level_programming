b93dfd0c2dceedcf5487fa1d3f7d9ce1da950deb
« Ephely » <ephely1@gmail.com>
2024-11-21 14:16:08 +0000

    index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -7,6 +7,7 @@
 * @size: int
 * @cmp: void
 *
+* Return: return
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -14,15 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 int i;
 
-if (array == NULL || cmp == NULL)
+if (array == NULL || cmp == NULL || size < 1)
 return (-1);
 
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
-{
 return (i);
-}
 }
 return (-1);
 }
