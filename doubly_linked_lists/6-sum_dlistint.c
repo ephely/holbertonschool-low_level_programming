1c6dbce69cf6a7c033a73ff1fc3a0c7459936ced
« Ephely » <ephely1@gmail.com>
2024-12-06 12:27:14 +0100

    sum

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * sum_dlistint - returns the sum of all the data 
+ * sum_dlistint - returns the sum of all the data
  * (n) of a dlistint_t linked list.
  *
  * @head: head
- * 
+ *
  * Return: sum
  */
 
